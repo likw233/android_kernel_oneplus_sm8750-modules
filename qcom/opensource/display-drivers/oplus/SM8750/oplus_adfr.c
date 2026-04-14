5b9deaa5b48e61c6cc6ae8c98d0d702893be99bb
likw <1912283462@qq.com>
2026-04-14 22:47:07 +0800

    display: oplus: Force enable standalone ADFR and 1Hz min fps  for AOSP
    In custom ROM, SurfaceFlinger lacks the proprietary
    Oplus properties and callbacks to properly trigger the hardware
    ADFR (Auto Dynamic Frame Rate) engine. This causes the display to
    lock at 120Hz/60Hz.
    
    This commit forcefully overrides the ADFR status reset and SA
    (Standalone) handle logic:
    1. Hardcodes `sa_min_fps` to 1.
    2. Enforces `auto_mode` to `OPLUS_ADFR_AUTO_ON`.
    
    By doing this, we bypass the framework restrictions and let the DDIC
    hardware handle the 1-120Hz LTPO switching automatically when the
    panel is idle.
    
    Change-Id: If669bcdc3646dc111c2be98bd62c9ccff30a1fa6
    
    Change-Id: Id84782320d0225358522bc0c83c374886e10ece9
    Signed-off-by: klozz <carlosj@klozz.dev>

--- qcom/opensource/display-drivers/oplus/SM8750/oplus_adfr.c
+++ qcom/opensource/display-drivers/oplus/SM8750/oplus_adfr.c
@@ -1789,8 +1789,8 @@ int oplus_adfr_status_reset(void *dsi_panel)
 		ADFR_DEBUG("oplus_adfr_need_filter_auto_on_cmd:%d\n", p_oplus_adfr_params->need_filter_auto_on_cmd);
 		OPLUS_ADFR_TRACE_INT("oplus_adfr_need_filter_auto_on_cmd", p_oplus_adfr_params->need_filter_auto_on_cmd);
 
-		p_oplus_adfr_params->sa_min_fps = refresh_rate;
-		p_oplus_adfr_params->sa_min_fps_updated = false;
+		p_oplus_adfr_params->sa_min_fps = 1;
+		p_oplus_adfr_params->sa_min_fps_updated = true;
 		if (oplus_adfr_high_precision_sa_mode_is_enabled(p_oplus_adfr_params)) {
 			p_oplus_adfr_params->sa_high_precision_fps_updated = false;
 		}
@@ -1834,6 +1834,14 @@ int oplus_adfr_status_reset(void *dsi_panel)
 	OPLUS_ADFR_TRACE_END("oplus_adfr_status_reset");
 
 	ADFR_DEBUG("end\n");
+
+	if (p_oplus_adfr_params) {
+		p_oplus_adfr_params->sa_min_fps = 1;
+		p_oplus_adfr_params->sa_min_fps_updated = true;
+
+		p_oplus_adfr_params->auto_mode = OPLUS_ADFR_AUTO_ON;
+		p_oplus_adfr_params->auto_mode_updated = true;
+	}
 
 	return rc;
 }
