6b5c4f388ceff28ce8f027f09adb1b99dd3841cb
likw <1912283462@qq.com>
2026-06-12 12:36:09 +0800

    Revert "display: oplus: Enable standalone ADFR 1Hz idle for AOSP"
    This reverts commit 283f7fa714fa68cc2e29c9159dc86ec1f55d479e.

--- qcom/opensource/display-drivers/oplus/SM8750/oplus_adfr.c
+++ qcom/opensource/display-drivers/oplus/SM8750/oplus_adfr.c
@@ -1024,14 +1024,6 @@ int oplus_adfr_property_update(void *sde_connector, void *sde_connector_state, i
 		return -EINVAL;
 	}
 
-	p_oplus_adfr_params->sa_min_fps = 1;
-	p_oplus_adfr_params->sa_min_fps_updated = true;
-	prop_val = OPLUS_ADFR_SA_MAGIC
-        | OPLUS_ADFR_AUTO_MODE_MAGIC
-        | (OPLUS_ADFR_AUTO_ON << 16)
-        | OPLUS_ADFR_SA_MIN_FPS_MAGIC
-        | 1;
-
 	if (!oplus_adfr_is_supported(p_oplus_adfr_params)) {
 		ADFR_DEBUG("adfr is not supported\n");
 		return 0;
@@ -1053,8 +1045,6 @@ int oplus_adfr_property_update(void *sde_connector, void *sde_connector_state, i
 			handled = BIT(0);
 
 			if (prop_val & OPLUS_ADFR_AUTO_MODE_MAGIC) {
-				p_oplus_adfr_params->auto_mode = OPLUS_ADFR_AUTO_ON;
-				p_oplus_adfr_params->auto_mode_updated = true;
 				if (p_oplus_adfr_params->need_filter_auto_on_cmd
 						&& (OPLUS_ADFR_AUTO_MODE_VALUE(prop_val) == OPLUS_ADFR_AUTO_ON)) {
 					ADFR_INFO("auto off cmds and auto on cmds could not be sent in the same frame, filter it out\n");
@@ -1795,14 +1785,12 @@ int oplus_adfr_status_reset(void *dsi_panel)
 	if ((h_skew == STANDARD_ADFR) || (h_skew == STANDARD_MFR)) {
 		p_oplus_adfr_params->auto_mode = OPLUS_ADFR_AUTO_OFF;
 		/* after auto off cmd was sent, auto on cmd filter start */
-		p_oplus_adfr_params->auto_mode = OPLUS_ADFR_AUTO_ON;
-		p_oplus_adfr_params->auto_mode_updated = true;
-		p_oplus_adfr_params->need_filter_auto_on_cmd = false;
+		p_oplus_adfr_params->need_filter_auto_on_cmd = true;
 		ADFR_DEBUG("oplus_adfr_need_filter_auto_on_cmd:%d\n", p_oplus_adfr_params->need_filter_auto_on_cmd);
 		OPLUS_ADFR_TRACE_INT("oplus_adfr_need_filter_auto_on_cmd", p_oplus_adfr_params->need_filter_auto_on_cmd);
 
-		p_oplus_adfr_params->sa_min_fps = 1;
-		p_oplus_adfr_params->sa_min_fps_updated = true;
+		p_oplus_adfr_params->sa_min_fps = refresh_rate;
+		p_oplus_adfr_params->sa_min_fps_updated = false;
 		if (oplus_adfr_high_precision_sa_mode_is_enabled(p_oplus_adfr_params)) {
 			p_oplus_adfr_params->sa_high_precision_fps_updated = false;
 		}
@@ -1846,14 +1834,7 @@ int oplus_adfr_status_reset(void *dsi_panel)
 	OPLUS_ADFR_TRACE_END("oplus_adfr_status_reset");
 
 	ADFR_DEBUG("end\n");
-	if (p_oplus_adfr_params) {
-		p_oplus_adfr_params->sa_min_fps = 1;
-		p_oplus_adfr_params->sa_min_fps_updated = true;
-		if (p_oplus_adfr_params->auto_mode != OPLUS_ADFR_AUTO_ON) {
-			p_oplus_adfr_params->auto_mode = OPLUS_ADFR_AUTO_ON;
-			p_oplus_adfr_params->auto_mode_updated = true;
-		}
-	}
+
 	return rc;
 }
 
